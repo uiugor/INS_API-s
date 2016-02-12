bd2c84707d4b45806659fb166d67a9b7f70efd73
uiugor <igor_confetti@hotmail.com>
2016-02-12 15:59:01 -0200

    Update whatsapp.php
    noem

--- examples/whatsapp.php
+++ examples/whatsapp.php
@@ -825,7 +825,7 @@ class Whatsapp
 			<head>
 				<meta charset="utf-8">
 				<meta http-equiv="X-UA-Compatible" content="IE=edge">
-				<title>Personal Whatsapp</title>
+				<title>Sistema Whatsapp Inside</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta name="description" content="">
 				<meta name="author" content="">
