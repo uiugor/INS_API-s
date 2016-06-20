d0aec6f053879759a6a6e9d3912cce6b18378fc5
uiugor <igor_confetti@hotmail.com>
2016-06-20 14:50:53 -0300

    Alterado Metodos e nomes
    Captivate

--- internalserverread.php
+++ internalserverread.php
@@ -1,21 +1,22 @@
-<?php
+<?php #Leitura dos Resultados#
+# Editado e Remodelado por Igor Confetti - Inside Sistemas#
 switch($_POST['API'])
 {
 	case 1: getCompanies();
 			break;
-	case 2: getDepartments($_POST['company']);
+	case 2: getDepartments($_POST['Empresa']);
 			break;
-	case 3: getCourses($_POST['company'],$_POST['department']);
+	case 3: getCourses($_POST['Empresa'],$_POST['Departamento']);
 			break;
-	case 4: getXMLs($_POST['company'],$_POST['department'],$_POST['course']);
+	case 4: getXMLs($_POST['Empresa'],$_POST['Departamento'],$_POST['Modulo']);
 			break;
-	case 5: downloadXML($_POST['company'],$_POST['department'],$_POST['course'],$_POST['xmlname']);
+	case 5: downloadXML($_POST['Empresa'],$_POST['Departamento'],$_POST['Modulo'],$_POST['xmlname']);
 			break;
 	default:break;
 }
 function getCompanies()
 {
-	$dir = @opendir("CaptivateResults");
+	$dir = @opendir("ResultadosCaptivate");
 	if($dir != "")
 	{
 		while(($file = readdir($dir)) !== false)
@@ -26,11 +27,11 @@ function getCompanies()
 		closedir($dir);
 	}
 	else
-	echo "No Captivate Results found;";
+	echo "Nenhum Resultado Encontrado;";
 }
 function getDepartments($comp)
 {
-	$dir = @opendir("CaptivateResults"."/".$comp);
+	$dir = @opendir("ResultadosCaptivate"."/".$comp);
 	while (($file = readdir($dir)) !== false)
 	{
 		if(!is_file($file))
@@ -40,7 +41,7 @@ function getDepartments($comp)
 }
 function getCourses($comp,$dept)
 {
-	$dir = @opendir("CaptivateResults"."/".$comp."/".$dept);
+	$dir = @opendir("ResultadosCaptivate"."/".$comp."/".$dept);
 	while (($file = readdir($dir)) !== false)
 	{
 		if(!is_file($file))
@@ -50,8 +51,8 @@ function getCourses($comp,$dept)
 }
 function getXMLs($comp,$dept,$course)
 {
-	$dir = @opendir("CaptivateResults"."/".$comp."/".$dept."/".$course);
-	$directory = "CaptivateResults"."/".$comp."/".$dept."/".$course;
+	$dir = @opendir("ResultadosCaptivate"."/".$comp."/".$dept."/".$course);
+	$directory = "ResultadosCaptivate"."/".$comp."/".$dept."/".$course;
 	while (($file = readdir($dir)) !== false)
 	{
 		if(!(is_dir($file)) && findexts($file) == 'xml')
@@ -63,7 +64,7 @@ function getXMLs($comp,$dept,$course)
 }
 function downloadXML($comp,$dept,$course,$name)
 {
-	$dir = "CaptivateResults"."/".$comp."/".$dept."/".$course."/".$name;
+	$dir = "ResultadosCaptivate"."/".$comp."/".$dept."/".$course."/".$name;
 	$handle = fopen($dir, "r");
 	$contents = fread($handle, filesize($dir));
 	fclose($handle);
